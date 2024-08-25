a645453e8f5f17a28d486adb77102d1162b0afc1
Hossein Khezeli <123948444+hosseinkhezeli@users.noreply.github.com>
2024-08-26 03:00:36 +0330

    refactor:performance and clean up

--- src/components/ProvidersLayout.tsx
+++ src/components/ProvidersLayout.tsx
@@ -1,28 +1,36 @@
 'use client';
-import React, { FC, ReactNode } from 'react';
-import ThemeProvider from '@styles/theme/ThemeProvider';
+//@3rd Party
+import { FC, ReactNode, useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
-
 import { SnackbarProvider } from 'notistack';
-import { Locale } from '@/i18n';
 import { store } from '@states/store';
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Components
+import ThemeProvider from '@styles/theme/ThemeProvider';
 import CustomLoadingIndicator from '@components/custom-loading/CustomLoadingIndicator';
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//@Types
+import { Locale } from '@/i18n';
 export interface IProvidersLayout {
     children: ReactNode;
     lang: Locale;
 }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const ProvidersLayout: FC<IProvidersLayout> = ({ children, lang }) => {
     const queryClient = new QueryClient();
+    const persistor = persistStore(store);
     return (
         <QueryClientProvider client={queryClient}>
             <Provider store={store}>
                 <PersistGate
                     loading={<CustomLoadingIndicator />}
-                    persistor={persistStore(store)}
+                    persistor={persistor}
                 >
                     <ThemeProvider lang={lang}>
                         <SnackbarProvider>{children}</SnackbarProvider>
