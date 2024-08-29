833f118ddeea18e2bfef26887ccc4087f6cc7207
Hossein Khezeli <123948444+hosseinkhezeli@users.noreply.github.com>
2024-08-29 03:44:48 +0330

    feat:404 custom error

--- src/app/[lang]/[...not_found]/page.tsx
+++ src/app/[lang]/[...not_found]/page.tsx
@@ -1,3 +1,47 @@
+import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
+
 export default function NotFoundCatchAll() {
-  return <p>Page Not Found</p>;
+    return (
+        <Box
+            width='100%'
+            height='100%'
+            display={'flex'}
+            flexDirection={'column'}
+            sx={{ placeContent: 'center', alignItems: 'center' }}
+        >
+            <Typography
+                style={{
+                    fontSize: '95px',
+                    letterSpacing: '-20px',
+                    fontWeight: 900,
+                    opacity: '0.1',
+                }}
+            >
+                404
+            </Typography>
+            <Typography
+                style={{
+                    fontSize: '95px',
+                    letterSpacing: '-20px',
+                    fontWeight: 900,
+                    opacity: '0.5',
+                }}
+            >
+                404
+            </Typography>
+            <Typography
+                style={{
+                    fontSize: '95px',
+                    letterSpacing: '-20px',
+                    fontWeight: 900,
+                }}
+            >
+                404
+            </Typography>
+            <Typography mt={'1rem'}>
+                Well, this page must’ve gone for a coffee break! ☕
+            </Typography>
+        </Box>
+    );
 }
