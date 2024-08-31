8d0565cd9ad1c4aeb0eb2992632aa465f9e36d45
Hossein Khezeli <123948444+hosseinkhezeli@users.noreply.github.com>
2024-09-01 01:32:37 +0330

    feat:add login process

--- src/app/[lang]/(authentication)/sign-in/components/SignInForm.tsx
+++ src/app/[lang]/(authentication)/sign-in/components/SignInForm.tsx
@@ -6,65 +6,62 @@ import { Button, Stack, Typography } from '@mui/material';
 import CustomForm from '@components/custom-form-generator/CustomForm';
 import useSignInForm from '@/app/[lang]/(authentication)/sign-in/hooks/useSignInForm';
 import { cominIn, cominUp } from '@utils/animationKeyframes';
-import Href from '@components/custom-link/CustomLink';
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //@Types
 import { TAuth } from '@i18n/dictionary/types/auth';
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const SignInForm = ({ dictionary }: { dictionary: TAuth }) => {
-    const { onSubmit, signInForm, inputList } = useSignInForm({ dictionary });
+    // Destructure the custom hook for handling sign-in logic
+    const {
+        onSubmitOtp,
+        otpForm,
+        inputListOtp,
+        onSubmitSignIn,
+        signInForm,
+        inputListSignIn,
+        step,
+    } = useSignInForm({ dictionary });
 
     return (
-            <Stack
-                    sx={{
-                        backgroundColor: 'background.default',
-                        p: { xs: '1rem 15%', sm: '0 10%' },
-                        flexBasis: '0%',
-                        maxWidth: { xs: '100%', sm: '50%' },
-                        animation: {
-                            xs: `${cominUp} 0.5s ease forwards`,
-                            sm: `${cominIn} 0.5s ease forwards`
-                        },
-                        borderRadius: {
-                            xs: '24px 24px 0 0',
-                            sm: '0px 24px 24px 0px'
-                        },
-                        justifyContent: 'center',
-                        alignItems: 'center'
-                    }}
+        <Stack
+            sx={{
+                backgroundColor: 'background.default',
+                p: { xs: '1rem 15%', sm: '0 10%' },
+                flexBasis: '0%',
+                maxWidth: { xs: '100%', sm: '50%' },
+                animation: {
+                    xs: `${cominUp} 0.5s ease forwards`,
+                    sm: `${cominIn} 0.5s ease forwards`,
+                },
+                borderRadius: {
+                    xs: '24px 24px 0 0',
+                    sm: '0px 24px 24px 0px',
+                },
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <Typography variant='h6' sx={{ textWrap: 'nowrap' }}>
+                {`${dictionary.welcome_back}, ${dictionary.mate}!`}
+            </Typography>
+            <CustomForm
+                form={step === 0 ? otpForm : signInForm}
+                onSubmit={step === 0 ? onSubmitOtp : onSubmitSignIn}
+                formConfig={step === 0 ? inputListOtp : inputListSignIn}
             >
-                <Typography variant={'h6'} sx={{ textWrap: 'nowrap' }}>
-                    {dictionary.welcome_back},{dictionary.mate}!
-                </Typography>
-                <CustomForm
-                        form={signInForm}
-                        onSubmit={onSubmit}
-                        formConfig={inputList}
-                >
-                    <Stack gap={1}>
-
-                        <Button
-                                type="submit"
-                                variant="contained"
-                                color="primary"
-                                fullWidth
-                        >
-                            {dictionary.sign_in}
-                        </Button>
-                        <Href to={'/sign-up'}>
-
-                            <Button
-                                    variant="outlined"
-                                    color="primary"
-                                    fullWidth
-                            >
-                                {dictionary.sign_up}
-                            </Button>
-                        </Href>
-                    </Stack>
-                </CustomForm>
-            </Stack>
+                <Stack gap={1}>
+                    <Button
+                        type='submit'
+                        variant='contained'
+                        color='primary'
+                        fullWidth
+                    >
+                        {step === 0 ? dictionary.send_code : dictionary.sign_in}
+                    </Button>
+                </Stack>
+            </CustomForm>
+        </Stack>
     );
 };
 
--- src/i18n/dictionary/fa/sections/fa_auth.ts
+++ src/i18n/dictionary/fa/sections/fa_auth.ts
@@ -5,9 +5,10 @@ export const fa_auth: TAuth = {
   mate: 'رفیق',
   sign_in: 'ورود',
   phone_number: 'شماره همراه',
-  sign_up:'ثبت نام',
-  welcome:'به کایزن خوش اومدی',
-  email:"ایمیل",
-  fullName:'نام و نام خانوادگی',
-  userName:'نام کاربری'
+  sign_up: 'ثبت نام',
+  welcome: 'به کایزن خوش اومدی',
+  email: 'ایمیل',
+  fullName: 'نام و نام خانوادگی',
+  userName: 'نام کاربری',
+  send_code: 'ارسال کد',
 };
