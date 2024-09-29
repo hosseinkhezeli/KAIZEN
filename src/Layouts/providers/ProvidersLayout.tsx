ce5896d5a312a15d3518c672503dd18ba328cc16
Hossein Khezeli <123948444+hosseinkhezeli@users.noreply.github.com>
2024-09-30 01:10:53 +0330

    fix:clean up

--- src/Layouts/providers/ProvidersLayout.tsx
+++ src/Layouts/providers/ProvidersLayout.tsx
@@ -7,12 +7,14 @@ import { SnackbarProvider } from 'notistack';
 
 //Components
 import ThemeProvider from '@styles/theme/ThemeProvider';
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //@Types
 import { Locale } from '@/i18n';
-import AuthLayout from '@/Layouts/AuthLayout';
+import AuthLayout from '@/layouts/providers/AuthLayout';
 import useUserStore from '@states/user/userSlice';
+import { useTheme } from '@mui/material/styles';
 export interface IProvidersLayout {
     children: ReactNode;
     lang: Locale;
@@ -22,7 +24,7 @@ export interface IProvidersLayout {
 const ProvidersLayout: FC<IProvidersLayout> = ({ children, lang }) => {
     const queryClient = new QueryClient();
     const { initialize } = useUserStore();
-
+    const theme = useTheme();
     useEffect(() => {
         initialize();
     }, [initialize]);
@@ -31,6 +33,12 @@ const ProvidersLayout: FC<IProvidersLayout> = ({ children, lang }) => {
             <AuthLayout lang={lang}>
                 <ThemeProvider lang={lang}>
                     <SnackbarProvider>{children}</SnackbarProvider>
+                    <ProgressBar
+                        height='4px'
+                        color={theme.palette.warning.main}
+                        options={{ showSpinner: true }}
+                        shallowRouting
+                    />
                 </ThemeProvider>
             </AuthLayout>
         </QueryClientProvider>
